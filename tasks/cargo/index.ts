89514b7f7b6b6f90ce225c11f263009a79e775af
spontoreau <pontoreau.sylvain@gmail.com>
2018-06-21 22:16:06 +0200

    Change implementation. Use task library to check if rustup is available

--- tasks/cargo/index.ts
+++ tasks/cargo/index.ts
@@ -1,5 +1,11 @@
+import tl from "vsts-task-lib";
 import checkRustup from "./checkRustup";
 
 (async () => {
-    await checkRustup();
+    try {
+        const rustupAvailable = await checkRustup();
+        tl.debug(`${rustupAvailable}`);
+    } catch(err) {
+        tl.error(err);
+    }
 })();
