a4d5e520a9c7d745d1f114e4cb29ae12d8a88b6c
spontoreau <pontoreau.sylvain@gmail.com>
2018-06-22 20:15:27 +0200

    Update Rust if Rustup is available

--- tasks/install/index.ts
+++ tasks/install/index.ts
@@ -1,12 +1,19 @@
 import tl from "vsts-task-lib";
 
-const isRustupAvailable = () => tl.which("rustup") ? Promise.resolve(true) : Promise.resolve(false);
-
 (async () => {
     try {
-        const rustupAvailable = await isRustupAvailable();
-        tl.debug(`${rustupAvailable}`);
-    } catch(err) {
-        tl.error(err);
+        if(!tl.which("rustup")) {
+            console.log(`Rustup not available.`);
+        } else {
+            const returnCode = await tl.exec("rustup", "update");
+
+            if(returnCode > 0) {
+                tl.setResult(tl.TaskResult.Failed, "Rustup update failed.");
+            } else {
+                tl.setResult(tl.TaskResult.Succeeded, "Rust updated.");
+            }
+        }
+    } catch(e) {
+        tl.setResult(tl.TaskResult.Failed, e.message);
     }
 })();
