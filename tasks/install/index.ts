3e84a273f9e7dd76c862281070ca862899cb7f97
spontoreau <pontoreau.sylvain@gmail.com>
2018-06-25 17:23:59 +0200

    Refactor downloadAndInstall function

--- tasks/install/index.ts
+++ tasks/install/index.ts
@@ -14,12 +14,14 @@ import { exec, setResult, TaskResult, which, tool } from "vsts-task-lib";
 
 async function downloadAndInstall() {
     console.log(`Rustup not available.`);
-    const curl = tool(which("curl"));
-    curl.arg("https://sh.rustup.rs").arg("-sSf");
-    const sh = tool(which("sh"));
-    sh.arg("-s").arg("--").arg("-y");
-    curl.pipeExecOutputToTool(sh);
-    const updated = await curl.exec() === 0;
+    const updated = await tool(which("curl"))
+            .arg("https://sh.rustup.rs")
+            .arg("-sSf")
+            .pipeExecOutputToTool(tool(which("sh"))
+                                    .arg("-s")
+                                    .arg("--")
+                                    .arg("-y")
+            ).exec() === 0;
     setUpdateResult(updated);
 }
 
