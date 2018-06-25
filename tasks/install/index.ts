90549469247d8802c5d56ef97e2ca8eb02728bf4
spontoreau <pontoreau.sylvain@gmail.com>
2018-06-25 17:10:12 +0200

    Download rustup if not available, then install Rust latest version. Unix only

--- tasks/install/index.ts
+++ tasks/install/index.ts
@@ -1,21 +1,36 @@
-import { exec, setResult, TaskResult, which } from "vsts-task-lib";
+import { exec, setResult, TaskResult, which, tool } from "vsts-task-lib";
 
 (async () => {
     try {
-        if(!which("rustup")) {
+        if (!which("rustup")) {
             console.log(`Rustup not available.`);
+
+            const curl = tool(which("curl"));
+            curl.arg("https://sh.rustup.rs").arg("-sSf");
+
+            const sh = tool(which("sh"));
+            sh.arg("-s").arg("--").arg("-y");
+
+            curl.pipeExecOutputToTool(sh);
+
+            const updated = await curl.exec() === 0;
+
+            setResult(
+                updated ? TaskResult.Succeeded : TaskResult.Failed,
+                updated ? "Rust updated." : "Rustup update failed."
+            )
         } else {
             await update();
         }
-    } catch(e) {
+    } catch (e) {
         setResult(TaskResult.Failed, e.message);
     }
 })();
 
 async function update() {
-    const updated = await exec("rustup", "update") > 0;
+    const updated = await exec("rustup", "update") === 0;
     setResult(
-        updated ? TaskResult.Succeeded : TaskResult.Succeeded, 
+        updated ? TaskResult.Succeeded : TaskResult.Failed,
         updated ? "Rust updated." : "Rustup update failed."
     );
 }
