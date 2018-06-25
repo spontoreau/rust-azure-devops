9d61941f7b877a7a3b43b21452a2c21e77769cad
spontoreau <pontoreau.sylvain@gmail.com>
2018-06-25 17:18:26 +0200

    Extract downloadAndInstall function

--- tasks/install/index.ts
+++ tasks/install/index.ts
@@ -3,18 +3,7 @@ import { exec, setResult, TaskResult, which, tool } from "vsts-task-lib";
 (async () => {
     try {
         if (!which("rustup")) {
-            console.log(`Rustup not available.`);
-
-            const curl = tool(which("curl"));
-            curl.arg("https://sh.rustup.rs").arg("-sSf");
-
-            const sh = tool(which("sh"));
-            sh.arg("-s").arg("--").arg("-y");
-
-            curl.pipeExecOutputToTool(sh);
-
-            const updated = await curl.exec() === 0;
-            setUpdateResult(updated);
+            await downloadAndInstall();
         } else {
             await update();
         }
@@ -22,6 +11,17 @@ import { exec, setResult, TaskResult, which, tool } from "vsts-task-lib";
         setResult(TaskResult.Failed, e.message);
     }
 })();
+
+async function downloadAndInstall() {
+    console.log(`Rustup not available.`);
+    const curl = tool(which("curl"));
+    curl.arg("https://sh.rustup.rs").arg("-sSf");
+    const sh = tool(which("sh"));
+    sh.arg("-s").arg("--").arg("-y");
+    curl.pipeExecOutputToTool(sh);
+    const updated = await curl.exec() === 0;
+    setUpdateResult(updated);
+}
 
 async function update() {
     const updated = await exec("rustup", "update") === 0;
