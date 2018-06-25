bcb0942ade5f06fb625cba330049dbadd720756b
spontoreau <pontoreau.sylvain@gmail.com>
2018-06-25 15:25:24 +0200

    Extract rustup update in a fonction

--- tasks/install/index.ts
+++ tasks/install/index.ts
@@ -5,14 +5,17 @@ import { exec, setResult, TaskResult, which } from "vsts-task-lib";
         if(!which("rustup")) {
             console.log(`Rustup not available.`);
         } else {
-            const updated = await exec("rustup", "update") > 0;
-
-            setResult(
-                updated ? TaskResult.Succeeded : TaskResult.Succeeded,
-                updated ? "Rust updated." : "Rustup update failed."
-            );
+            await update();
         }
     } catch(e) {
         setResult(TaskResult.Failed, e.message);
     }
 })();
+
+async function update() {
+    const updated = await exec("rustup", "update") > 0;
+    setResult(
+        updated ? TaskResult.Succeeded : TaskResult.Succeeded, 
+        updated ? "Rust updated." : "Rustup update failed."
+    );
+}
