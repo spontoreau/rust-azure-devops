a2125a53161aa9b356614b851a2e47bfe2117be3
spontoreau <pontoreau.sylvain@gmail.com>
2018-07-06 18:18:47 +0200

    Cargo task refactoring

--- tasks/cargo/index.ts
+++ tasks/cargo/index.ts
@@ -12,23 +12,16 @@ import {
     ToolRunner
 } from "vsts-task-lib/toolrunner";
 
-
 (async () => {
     try {
         const command = getInput("cargoCommand");
         const args = getInput("cargoCommandArguments");
 
-        if (!!which("cargo")) {
-            const returnCode = await exec("cargo", [command, ...args.split(" ")]);
-            if (returnCode > 0) {
-                setResult(TaskResult.Failed, "Error");
-            }
-            else {
-                setResult(TaskResult.Succeeded, "Task done!");
-            }
-        } else {
-            setResult(TaskResult.Failed, "Cargo is not available");
-        }
+        !!which("cargo") 
+            ? await await exec("cargo", [command, ...args.split(" ")]) > 0 
+                ? setResult(TaskResult.Failed, "Error")
+                : setResult(TaskResult.Succeeded, "Task done!")
+            : setResult(TaskResult.Failed, "Cargo is not available")
     } catch (e) {
         setResult(TaskResult.Failed, e.message);
     }
