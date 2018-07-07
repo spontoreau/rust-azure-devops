859ca5bd6a42c23792ce048358296aa12693f0cf
spontoreau <pontoreau.sylvain@gmail.com>
2018-07-07 14:32:32 +0200

    Resolve arguments issue

---
+++ debug/cargo/noArgs.ts
@@ -0,0 +1,8 @@
+import { join } from "path";
+import { TaskMockRunner } from "vsts-task-lib/mock-run";
+
+const taskPath = join(__dirname, '../../tasks/cargo', 'index.js');
+
+const taskMockRunner = new TaskMockRunner(taskPath);
+taskMockRunner.setInput("cargoCommand", "help");
+taskMockRunner.run();
