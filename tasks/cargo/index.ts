8720f1b70c80cd365cd9abde36a464a2b081f9d2
spontoreau <pontoreau.sylvain@gmail.com>
2018-07-07 18:30:58 +0200

    Add Cargo in path for every OS

--- tasks/cargo/index.ts
+++ tasks/cargo/index.ts
@@ -1,4 +1,5 @@
 import {
+    debug,
     exec,
     getInput,
     setResult,
@@ -6,15 +7,39 @@ import {
     which,
 } from "vsts-task-lib";
 
+import os from "os";
+import process from "process";
+
 (async () => {
     try {
+        const separator = os.platform() === "win32" ? ";" : ":";
+        console.log(`Separator: ${separator}`);
+        console.log(`OS: ${os.platform()}`);
+        const cargoPath = ((system: string) => {
+            switch(system) {
+                case "win32":
+                    return "c:/Users/VssAdministrator/.cargo/bin";
+                case "darwin":
+                    return "/Users/vsts/.cargo/bin";
+                case "linux":
+                    return "/root/.cargo/bin";
+            }
+        })(os.platform());
+        console.log(`Cargo path: ${cargoPath}`);
+        const currentPath = process.env["PATH"];
+        process.env["PATH"] = process.env["PATH"]
+            ? cargoPath + separator + currentPath
+            : cargoPath;
+
+        console.log(process.env["PATH"]);
+
         const command = getInput("cargoCommand");
         const argsInput = getInput("cargoCommandArguments");
 
-        const args = argsInput 
+        const args = argsInput
             ? [command, ...argsInput.split(" ")]
             : command;
-            
+
         which("cargo")
             ? await await exec("cargo", args) > 0
                 ? setResult(TaskResult.Failed, "Error")
