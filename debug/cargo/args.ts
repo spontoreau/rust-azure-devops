ca2b2a02a4907db9a9138c9ebd86796cd100b0f8
spontoreau <pontoreau.sylvain@gmail.com>
2018-07-13 10:50:14 +0200

    Move debug folder

--- debug/cargo/args.ts
+++ debug/cargo/args.ts
@@ -1,7 +1,7 @@
 import { join } from "path";
 import { TaskMockRunner } from "vsts-task-lib/mock-run";
 
-const taskPath = join(__dirname, "../../", "cargo.js");
+const taskPath = join(__dirname, "../../src", "cargo.js");
 
 const taskMockRunner = new TaskMockRunner(taskPath);
 taskMockRunner.setInput("cargoCommand", "search");
--- debug/cargo/noArgs.ts
+++ debug/cargo/noArgs.ts
@@ -1,7 +1,7 @@
 import { join } from "path";
 import { TaskMockRunner } from "vsts-task-lib/mock-run";
 
-const taskPath = join(__dirname, "../../", "index.js");
+const taskPath = join(__dirname, "../../src", "index.js");
 
 const taskMockRunner = new TaskMockRunner(taskPath);
 taskMockRunner.setInput("cargoCommand", "help");
