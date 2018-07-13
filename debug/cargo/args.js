c6d2a2d102ba1450c03a6227225aa18b5f18212c
spontoreau <pontoreau.sylvain@gmail.com>
2018-07-13 11:00:15 +0200

    Add vscode debug configuration

--- debug/cargo/args.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var path_1 = require("path");
-var mock_run_1 = require("vsts-task-lib/mock-run");
-var taskPath = path_1.join(__dirname, '../../', 'cargo.js');
-var taskMockRunner = new mock_run_1.TaskMockRunner(taskPath);
-taskMockRunner.setInput("cargoCommand", "search");
-taskMockRunner.setInput("cargoCommandArguments", "rocket --limit 1");
-taskMockRunner.run();
--- debug/cargo/noArgs.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var path_1 = require("path");
-var mock_run_1 = require("vsts-task-lib/mock-run");
-var taskPath = path_1.join(__dirname, '../../tasks/cargo', 'index.js');
-var taskMockRunner = new mock_run_1.TaskMockRunner(taskPath);
-taskMockRunner.setInput("cargoCommand", "help");
-taskMockRunner.run();
