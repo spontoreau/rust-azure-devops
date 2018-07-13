c3b26228d73272903cad21ba353fb6fad20a89ba
spontoreau <pontoreau.sylvain@gmail.com>
2018-07-13 11:04:28 +0200

    Rustup command execution

--- src/rustup.ts
+++ src/rustup.ts
@@ -1,12 +1,19 @@
 import {
+    getInput,
     setResult,
     TaskResult,
 } from "vsts-task-lib";
 
-(async () => {
-    try {
+import executeCommand from "./common/executeCommand";
 
+(async (command, args) => {
+    try {
+        await executeCommand(command, args);
     } catch (e) {
         setResult(TaskResult.Failed, e.message);
     }
-})();
+})(
+    getInput("rustupCommand"),
+    getInput("rustupCommandArguments"),
+);
+
