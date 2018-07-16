574acbbf06277b2126baa8a31173c000fcdc6638
spontoreau <pontoreau.sylvain@gmail.com>
2018-07-16 10:38:53 +0200

    Update executeCommand to handle execution result with Promise instead of setResult

--- src/cargo.ts
+++ src/cargo.ts
@@ -11,6 +11,7 @@ import executeCommand from "./common/executeCommand";
 (async (command, args) => {
     try {
         await executeCommand("cargo", command, args);
+        setResult(TaskResult.Succeeded, "Task done!");
     } catch (e) {
         setResult(TaskResult.Failed, e.message);
     }
