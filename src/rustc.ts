a1ff2c3e45c2d07839d1cb19c3a365317aaa405f
spontoreau <pontoreau.sylvain@gmail.com>
2018-07-21 16:47:11 +0200

    Rustc task refactoring

--- src/rustc.ts
+++ src/rustc.ts
@@ -2,31 +2,14 @@ import {
     getInput,
     setResult,
     TaskResult,
-    which,
-    exec,
 } from "vsts-task-lib";
 
 import executeCommand from "./common/executeCommand";
-import addCargoToPath from "./common/addCargoToPath";
 
 (async (options, input) => {
     try {
-        addCargoToPath();
-    
-        const toolArgs = options
-            ? [...options.split(" "), input]
-            : input;
-    
-        if(which("cargo")) {
-            const returnCode = await exec("rustc", toolArgs);
-            if(returnCode === 0) {
-                setResult(TaskResult.Succeeded, "Task done!");
-            } else {
-                setResult(TaskResult.Failed, "An error has occured.");
-            }
-        } else {
-            setResult(TaskResult.Failed, "Rust toolchains are not available.");
-        }
+        await executeCommand("rustc", input, options, true);
+        setResult(TaskResult.Succeeded, "Task done!");
     } catch (e) {
         setResult(TaskResult.Failed, e.message);
     }
