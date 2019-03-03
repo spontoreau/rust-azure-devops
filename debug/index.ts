425596129b2213a119c9a24907652a725418f48b
spontoreau <pontoreau.sylvain@gmail.com>
2019-03-03 22:49:53 +0100

    Add index.ts to launch all debug files

---
+++ debug/index.ts
@@ -0,0 +1,8 @@
+import "./install/default";
+import "./install/nigthly";
+import "./cargo/noOptions";
+import "./cargo/options";
+import "./rustc/noOptions";
+import "./rustc/options";
+import "./rustup/args";
+import "./rustup/noArgs";
